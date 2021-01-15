3d1410434639c3c6a252098f11b3f0bf4124db93
Yevhenii Filatov <yevgeniy.filatov97@gmail.com>
2021-01-15 13:24:05 +0200

    created google-serp project

---
+++ projects/google-serp/src/main/java/com/dataox/googleserp/GoogleSerpApplication.java
@@ -0,0 +1,13 @@
+package com.dataox.googleserp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSerpApplication.class, args);
+	}
+
+}
