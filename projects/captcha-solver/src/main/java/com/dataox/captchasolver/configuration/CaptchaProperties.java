29aa3c070f439ea1fb8f5c72d6570f2ec518a9f9
Georgii Sorokin <osiris94.04@gmail.com>
2021-01-21 18:00:28 +0200

    Remove useless import.

--- projects/captcha-solver/src/main/java/com/dataox/captchasolver/configuration/CaptchaProperties.java
+++ projects/captcha-solver/src/main/java/com/dataox/captchasolver/configuration/CaptchaProperties.java
@@ -3,7 +3,6 @@ package com.dataox.captchasolver.configuration;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 /**
  * @author Yevhenii Filatov
