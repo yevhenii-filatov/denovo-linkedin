ed93b2ce5d57586fb5658e71fa34d1cc44fba76a
EternalSadnes <waiting4miracle>
2021-03-16 11:51:21 +0200

    remove import

--- projects/linkedin-scraper/src/main/java/com/dataox/linkedinscraper/service/rabbitmq/RabbitmqListener.java
+++ projects/linkedin-scraper/src/main/java/com/dataox/linkedinscraper/service/rabbitmq/RabbitmqListener.java
@@ -8,7 +8,6 @@ import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
