dfc056da444bba8172a019600ce18b96e4aa695d
EternalSadnes <waiting4miracle>
2021-03-16 15:42:03 +0200

    fix bug with endorsements repository add datasource to properties add spring web to pom.xml

--- projects/load-balancer/src/main/java/com/dataox/loadbalancer/web/controller/ScrapingController.java
+++ projects/load-balancer/src/main/java/com/dataox/loadbalancer/web/controller/ScrapingController.java
@@ -1,8 +1,17 @@
 package com.dataox.loadbalancer.web.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Dmitriy Lysko
  * @since 16/03/2021
  */
+@RestController
 public class ScrapingController {
+
+    @GetMapping("/hi")
+    public String hello() {
+        return "Hello";
+    }
 }
