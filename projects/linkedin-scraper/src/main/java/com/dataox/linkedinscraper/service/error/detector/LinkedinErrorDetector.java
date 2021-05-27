e89b65eb4db919a846916b9939a1afde88f4c940
TheSlavaHero <theslavahero@gmail.com>
2021-05-27 15:31:00 +0200

    LinkedinErrorDetector logs added

--- projects/linkedin-scraper/src/main/java/com/dataox/linkedinscraper/service/error/detector/LinkedinErrorDetector.java
+++ projects/linkedin-scraper/src/main/java/com/dataox/linkedinscraper/service/error/detector/LinkedinErrorDetector.java
@@ -1,12 +1,16 @@
 package com.dataox.linkedinscraper.service.error.detector;
 
 import com.dataox.WebDriverUtils;
+import com.dataox.notificationservice.service.NotificationsService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
+@Data
 @Service
 public class LinkedinErrorDetector {
     private static final By UNEXPECTED_ERROR_ON_LINKEDIN_SIDE_SELECTOR = By.xpath("//h1[contains(text(),'Oops!')]");
@@ -23,41 +27,55 @@ public class LinkedinErrorDetector {
     private static final By LINKEDIN_LOGGED_OUT = By.xpath("//nav[@aria-label='Primary']//a[@class='nav__button-secondary'][text()='Sign in']");
     private static final By ADD_A_PHONE_NUMBER = By.xpath("//h2[text()='Add a phone number for security']");
 
+    private final NotificationsService notificationsService;
+
     public LinkedinError detect(WebDriver webDriver) {
         if (banDetected(webDriver)) {
+            notificationsService.sendAll("LinkedinScraper: An error Has been occurred: Linkedin account has been banned.");
             return LinkedinError.BANNED;
         }
         if (restrictionDetected(webDriver)) {
+            notificationsService.sendAll("LinkedinScraper: An error Has been occurred: Linkedin account has been restricted.");
             return LinkedinError.RESTRICTED;
         }
         if (emailVerificationDetected(webDriver)) {
+            notificationsService.sendAll("LinkedinScraper: An error Has been occurred: Linkedin account requires email verification.");
             return LinkedinError.EMAIL_VERIFICATION;
         }
         if (unexpectedErrorToastDetected(webDriver)) {
+            notificationsService.sendAll("LinkedinScraper: An error Has been occurred: Unknown error.");
             return LinkedinError.UNKNOWN_ERROR;
         }
         if (profileIsNotAvailable(webDriver)) {
+            notificationsService.sendAll("LinkedinScraper: An error Has been occurred: Profile is not available.");
             return LinkedinError.PROFILE_IS_NOT_AVAILABLE;
         }
         if (pageNotFound(webDriver)) {
+            notificationsService.sendAll("LinkedinScraper: An error Has been occurred: Page was not found.");
             return LinkedinError.PAGE_NOT_FOUND;
         }
         if (unexpectedErrorOnLinkedinSideDetected(webDriver)) {
+            notificationsService.sendAll("LinkedinScraper: An error Has been occurred: Unexpected error on Linkedin side.");
             return LinkedinError.OOPS_ITS_NOT_YOU_ITS_US;
         }
         if (dontHaveAccessToThisProfile(webDriver)) {
+            notificationsService.sendAll("LinkedinScraper: An error Has been occurred: Linkedin account does not have access to the profile.");
             return LinkedinError.DONT_HAVE_ACCESS_TO_PROFILE;
         }
         if (isntQuiteRight(webDriver)) {
+            notificationsService.sendAll("LinkedinScraper: An error Has been occurred: Something isn’t quite right.");
             return LinkedinError.ISNT_QUITE_RIGHT;
         }
         if (somethingWentWrong(webDriver)) {
+            notificationsService.sendAll("LinkedinScraper: An error Has been occurred: Something went wrong.");
             return LinkedinError.SOMETHING_WENT_WRONG;
         }
         if (linkedinLoggedOut(webDriver)) {
+            notificationsService.sendAll("LinkedinScraper: An error Has been occurred: Linkedin account was logged out.");
             return LinkedinError.LOGGED_OUT;
         }
         if (addAPhoneNumber(webDriver)) {
+            notificationsService.sendAll("LinkedinScraper: An error Has been occurred: Linkedin asks for a phone number for security.");
             return LinkedinError.ADD_A_PHONE_NUMBER;
         }
         return LinkedinError.NO_ERRORS;
